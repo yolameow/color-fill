cfe7c719f2d894bd9d1eb851d7e601a50fc9d247
Olga Yushkevich <olyal-ya@mail.ru>
2020-10-19 11:52:52 +0300

    final

--- color fill/ViewController.swift
+++ color fill/ViewController.swift
@@ -18,46 +18,51 @@ class ViewController: UIViewController {
     @IBOutlet var blueValueLabel: UILabel!
     
     @IBOutlet var currentColorView: UIView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         currentColorView.layer.cornerRadius = 20
         
-        redSlider.minimumTrackTintColor = .orange
-        blueSlider.minimumTrackTintColor = .orange
-        greenSlider.minimumTrackTintColor = .orange
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
         
         redValueLabel.text = String(redSlider.value)
         greenValueLabel.text = String(greenSlider.value)
         blueValueLabel.text = String(blueSlider.value)
-       
-        
     }
-
+    
     @IBAction func redSliderAction() {
         let redValue = CGFloat(redSlider.value)
         let greenValue = CGFloat(greenSlider.value)
         let blueValue = CGFloat(blueSlider.value)
+        
         redValueLabel.text = String(round(100*redSlider.value)/100)
-        currentColorView.tintColor = UIColor(displayP3Red: redValue, green: greenValue, blue: blueValue, alpha: redValue)
-    }
+        
+        currentColorView.backgroundColor = UIColor(displayP3Red: (redValue*255), green: (greenValue*255), blue: (blueValue*255), alpha: redValue)
+        }
     
     @IBAction func greenSliderAction() {
         let redValue = CGFloat(redSlider.value)
         let greenValue = CGFloat(greenSlider.value)
         let blueValue = CGFloat(blueSlider.value)
+        
         greenValueLabel.text = String(round(100*greenSlider.value)/100)
-        currentColorView.tintColor = UIColor(displayP3Red: redValue, green: greenValue, blue: blueValue, alpha: greenValue)
+        
+        currentColorView.backgroundColor = UIColor(displayP3Red: (redValue*255), green: (greenValue*255), blue: (blueValue*255), alpha: greenValue)
     }
     
     @IBAction func blueSliderAction() {
         let redValue = CGFloat(redSlider.value)
         let greenValue = CGFloat(greenSlider.value)
         let blueValue = CGFloat(blueSlider.value)
+        
         blueValueLabel.text = String(round(100*blueSlider.value)/100)
-        currentColorView.tintColor = UIColor(displayP3Red: redValue, green: greenValue, blue: blueValue, alpha: blueValue)
+        
+        currentColorView.backgroundColor = UIColor(displayP3Red: (redValue*255), green: (greenValue*255), blue: (blueValue*255), alpha: blueValue)
     }
     
 }
+
 
